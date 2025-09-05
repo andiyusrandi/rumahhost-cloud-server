52e0306ad33556b4f844be61ad50558ceb68fd46
andi@ <andiyusrandiyusuf@gmail.com>
2025-09-06 05:13:28 +0800

    up

---
+++ public/wp-content/uploads/uag-plugin/assets/0/uag-js-37-1663694054.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-8129369e' );
+	UAGBTabs.anchorTabId( '.uagb-block-8129369e' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-8129369e' );
+}, false );
+ })
